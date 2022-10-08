eb2eb19b7c58110425a5ef407a0c4e1503654a15
nqp2k3 <90236730+NQP27@users.noreply.github.com>
2022-10-08 16:01:30 +0700

    Update CTDL003.cpp

--- CTDL003.cpp
+++ CTDL003.cpp
@@ -11,10 +11,45 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int t;
-    cin >> t;
+    int t = 1;
     while (t--)
     {
+        int n, s;
+        cin >> n >> s;
+        int v[N], w[N];
+        string res;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> w[i];
+        }
+        for (int i = 1; i <= pow(2, n); i++)
+        {
+            string str = bitset<30>(i).to_string().substr(30 - n, n);
+            int sumv = 0, sumw = 0;
+            for (int i = 0; i < str.length(); i++)
+            {
+                if (str[i] == '1')
+                {
+                    sumv += v[i];
+                    sumw += w[i];
+                }
+            }
+            if (sumw <= s && sumv >= sum)
+            {
+                res = str;
+                sum = sumv;
+            }
+        }
+        cout << sum << endl;
+        for (int i = 0; i < res.length(); i++)
+        {
+            cout << res[i] << " ";
+        }
     }
     return 0;
 }
